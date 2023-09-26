80b9d489109f934b970c54ccf78df07f6e82ecb8
Fatheed7 <69724750+Fatheed7@users.noreply.github.com>
2023-09-26 13:05:42 +0100

    Initial commit

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+    let currentURL = tab.url;
+    let pattern = /^https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/blob\/main\/([^\/]+\.html)$/;
+    let match = currentURL.match(pattern);
+
+    if (match) {
+        let newURL = `http://htmlpreview.github.io/?${currentURL}`;
+        chrome.tabs.create({ url: newURL });
+    } else {
+        chrome.browserAction.setPopup({ tabId: tab.id, popup: "popup.html" });
+    }
+});
