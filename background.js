11e0fb399a9d0581e7f4d6486640cab2ef3c7a74
Fatheed7 <69724750+Fatheed7@users.noreply.github.com>
2023-10-04 19:57:55 +0100

    Initial Commit

--- background.js
+++ background.js
@@ -1,12 +1,23 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    let currentURL = tab.url;
-    let pattern = /https:\/\/github\.com\/[A-Za-z]+.*\.html/;
-    let match = currentURL.match(pattern);
-
-    if (match) {
-        let newURL = `http://htmlpreview.github.io/?${currentURL}`;
-        chrome.tabs.create({ url: newURL });
-    } else {
-        chrome.browserAction.setPopup({ tabId: tab.id, popup: "popup.html" });
-    }
+    chrome.tabs.executeScript(
+        { 
+            code: "document.getElementsByTagName('html')[0].outerHTML;",
+        },
+        function(code) {
+            let match = tab.url.startsWith("https://www.reddit.com/r/");
+            if (match) {
+                let regex = /https:\/\/v\.redd\.it\/[A-Za-z0-9]+\/HLSPlaylist\.m3u8/i;
+                let matches = code[0].match(regex);
+                if (matches) {
+                    let replacedUrls = matches.map(function(url) {
+                        return url.replace("HLSPlaylist.m3u8", "DASH_480.mp4");
+                    });
+                    chrome.tabs.create({ url: replacedUrls[0] });
+                } else {
+                    chrome.browserAction.setPopup({ tabId: tab.id, popup: "no_video.html" });
+                }
+            } else {
+                chrome.browserAction.setPopup({ tabId: tab.id, popup: "popup.html" });
+            }
+        })
 });
